301c0536ce7a68565c0dc30820ac6c738ac92a1a
Daniel Kuruc <daniel@kuruc.dev>
2023-02-15 18:40:10 +0100

    Make it configurable

--- pylint_google_style_guide_imports_enforcing/__init__.py
+++ pylint_google_style_guide_imports_enforcing/__init__.py
@@ -1,13 +1,17 @@
 import astroid
 from astroid import nodes
 
+from pylint import utils
 from pylint.checkers import BaseChecker
+from pylint.lint import PyLinter
 
 
 class ModuleOnlyImports(BaseChecker):
-    name = "check-if-we-import-only-modules"
-
+    DEFAULT_EXCLUDED_MODULES = {"typing", "typing_extensions", "six.moves", "collections.abc", "__future__"}
+    DEFAULT_EXCLUDED_NAMES = {"_", "__"}
     ONLY_IMPORTING_MODULES_IS_ALLOWED = "only-importing-modules-is-allowed"
+
+    name = "check-if-we-import-only-modules"
 
     msgs = {
         "C5101": (
@@ -16,19 +20,47 @@ class ModuleOnlyImports(BaseChecker):
             "",
         ),
     }
-    options = ()
+
+    options = (
+        (
+            "google-style-excluded-modules",
+            {
+                "default": "",
+                "type": "string",
+                "metavar": "<str>",
+                "help": "Comma-separated list of modules to ignore during check",
+            },
+        ),
+        (
+            "google-style-excluded-names",
+            {
+                "default": "",
+                "type": "string",
+                "metavar": "<str>",
+                "help": "Comma-separated list of import names to ignore during check",
+            },
+        ),
+    )
 
     priority = -1
 
-    excluded_modules = ["typing", "typing_extensions", "six.moves", "collections.abc", "__future__"]
 
     def visit_importfrom(self, node):
-        imported = node.do_import_module()
+        _excluded_modules = frozenset(
+            self.DEFAULT_EXCLUDED_MODULES | set(utils._splitstrip(self.linter.config.google_style_excluded_modules))
+        )
+        _excluded_names = frozenset(
+            self.DEFAULT_EXCLUDED_NAMES | set(utils._splitstrip(self.linter.config.google_style_excluded_names))
+        )
 
-        if imported.name in self.excluded_modules:
+        imported = node.do_import_module()
+        if imported.name in _excluded_modules:
             return
 
         for name, _ in node.names:
+            if name in _excluded_names:
+                continue
+
             _, result = imported.lookup(name)
             if not result:
                 # it's not there, so probably this is another module - fine.
